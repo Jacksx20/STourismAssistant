586bbf9376c7babe2581761b046c7c35d6be4c9d
Jack20 <chunjcsx20@vip.qq.com>
2026-05-21 21:15:22 +0800

    酒店推荐数优化

--- app.py
+++ app.py
@@ -244,7 +244,7 @@ def plan():
         
         food_recommendations = food_recommendations[:10]
         
-        hotel_keywords = ['酒店', '宾馆', '住宿']
+        hotel_keywords = ['酒店', '宾馆', '住宿', '民宿', '客栈', '度假酒店', '商务酒店', '精品酒店']
         hotel_recommendations = []
         seen_hotel_ids = set()
         
@@ -253,7 +253,7 @@ def plan():
                 hotel_result = client.search_poi(
                     keyword=keyword,
                     city=city,
-                    offset=20
+                    offset=40
                 )
                 hotel_list = parse_poi_list(hotel_result)
                 
@@ -263,12 +263,12 @@ def plan():
                         if hotel.get('rating') and hotel.get('photo'):
                             hotel_recommendations.append(hotel)
                 
-                if len(hotel_recommendations) >= 5:
+                if len(hotel_recommendations) >= 10:
                     break
             except:
                 continue
         
-        hotel_recommendations = hotel_recommendations[:5]
+        hotel_recommendations = hotel_recommendations[:10]
         
         if not all_spots:
             return jsonify({'ok': False, 'message': '未找到足够的景点信息'})
